c08f21bdde3a1040563e40075f83c598f7cd4d0c
Jonathan Reeves <jonathan.reeves.admi@corp.jasonsdeli.local>
2019-01-25 15:03:14 -0600

    Initial commit

---
+++ song.js
@@ -0,0 +1,22 @@
+/*
+ * Song Attributes list
+ *
+ * 
+ */
+
+var song        = "Fire";              // This is a string type
+var artist      = "Parabelle";
+var trackNumber = "2";                // This is a number type
+var albumName   = "AIR";
+var duration    = "2420";             // The length of the song in seconds
+var genre       = "Rock/Acoustic";
+var greatSong   = true;               // Great song. Very catchy
+
+
+console.log(song);
+console.log(artist);
+console.log(trackNumber);
+console.log(albumName);
+console.log(duration);
+console.log(genre);
+console.log(greatSong);
